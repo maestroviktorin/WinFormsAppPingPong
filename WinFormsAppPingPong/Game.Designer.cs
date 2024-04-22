5f06e68b7fcd3176110827866bac6c99a71a12fe
Viktor Starodubtsev <viktorst2005@gmail.com>
2024-04-22 12:24:14 +0400

    Add project files.

---
+++ WinFormsAppPingPong/Game.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WinFormsAppPingPong
+{
+    partial class Game
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
+            alien = new PictureBox();
+            cyborg = new PictureBox();
+            Timer = new System.Windows.Forms.Timer(components);
+            ball = new PictureBox();
+            cyborgTop = new PictureBox();
+            cyborgBottom = new PictureBox();
+            cyborgTopCorner = new PictureBox();
+            cyborgBottomCorner = new PictureBox();
+            alienBottomCorner = new PictureBox();
+            alienTopCorner = new PictureBox();
+            alienBottom = new PictureBox();
+            alienTop = new PictureBox();
+            cyborgFront = new PictureBox();
+            alienFront = new PictureBox();
+            cyborgCounterLabel = new Label();
+            alienCounterLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)alien).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cyborg).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ball).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cyborgTop).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cyborgBottom).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cyborgTopCorner).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cyborgBottomCorner).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)alienBottomCorner).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)alienTopCorner).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)alienBottom).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)alienTop).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cyborgFront).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)alienFront).BeginInit();
+            SuspendLayout();
+            // 
+            // alien
+            // 
+            alien.BackColor = Color.PaleGreen;
+            resources.ApplyResources(alien, "alien");
+            alien.BorderStyle = BorderStyle.FixedSingle;
+            alien.Name = "alien";
+            alien.TabStop = false;
+            // 
+            // cyborg
+            // 
+            cyborg.BackColor = Color.DarkOrchid;
+            resources.ApplyResources(cyborg, "cyborg");
+            cyborg.BorderStyle = BorderStyle.FixedSingle;
+            cyborg.Name = "cyborg";
+            cyborg.TabStop = false;
+            // 
+            // Timer
+            // 
+            Timer.Enabled = true;
+            Timer.Interval = 5;
+            Timer.Tick += Timer_Tick;
+            // 
+            // ball
+            // 
+            ball.BackColor = Color.Transparent;
+            resources.ApplyResources(ball, "ball");
+            ball.Name = "ball";
+            ball.TabStop = false;
+            // 
+            // cyborgTop
+            // 
+            cyborgTop.BackColor = Color.Indigo;
+            resources.ApplyResources(cyborgTop, "cyborgTop");
+            cyborgTop.Name = "cyborgTop";
+            cyborgTop.TabStop = false;
+            // 
+            // cyborgBottom
+            // 
+            cyborgBottom.BackColor = Color.Indigo;
+            resources.ApplyResources(cyborgBottom, "cyborgBottom");
+            cyborgBottom.Name = "cyborgBottom";
+            cyborgBottom.TabStop = false;
+            // 
+            // cyborgTopCorner
+            // 
+            cyborgTopCorner.BackColor = Color.Indigo;
+            resources.ApplyResources(cyborgTopCorner, "cyborgTopCorner");
+            cyborgTopCorner.Name = "cyborgTopCorner";
+            cyborgTopCorner.TabStop = false;
+            // 
+            // cyborgBottomCorner
+            // 
+            cyborgBottomCorner.BackColor = Color.Indigo;
+            resources.ApplyResources(cyborgBottomCorner, "cyborgBottomCorner");
+            cyborgBottomCorner.Name = "cyborgBottomCorner";
+            cyborgBottomCorner.TabStop = false;
+            // 
+            // alienBottomCorner
+            // 
+            alienBottomCorner.BackColor = Color.LimeGreen;
+            resources.ApplyResources(alienBottomCorner, "alienBottomCorner");
+            alienBottomCorner.Name = "alienBottomCorner";
+            alienBottomCorner.TabStop = false;
+            // 
+            // alienTopCorner
+            // 
+            alienTopCorner.BackColor = Color.LimeGreen;
+            resources.ApplyResources(alienTopCorner, "alienTopCorner");
+            alienTopCorner.Name = "alienTopCorner";
+            alienTopCorner.TabStop = false;
+            // 
+            // alienBottom
+            // 
+            alienBottom.BackColor = Color.LimeGreen;
+            resources.ApplyResources(alienBottom, "alienBottom");
+            alienBottom.Name = "alienBottom";
+            alienBottom.TabStop = false;
+            // 
+            // alienTop
+            // 
+            alienTop.BackColor = Color.LimeGreen;
+            resources.ApplyResources(alienTop, "alienTop");
+            alienTop.Name = "alienTop";
+            alienTop.TabStop = false;
+            // 
+            // cyborgFront
+            // 
+            cyborgFront.BackColor = Color.Indigo;
+            resources.ApplyResources(cyborgFront, "cyborgFront");
+            cyborgFront.Name = "cyborgFront";
+            cyborgFront.TabStop = false;
+            // 
+            // alienFront
+            // 
+            alienFront.BackColor = Color.LimeGreen;
+            resources.ApplyResources(alienFront, "alienFront");
+            alienFront.Name = "alienFront";
+            alienFront.TabStop = false;
+            // 
+            // cyborgCounterLabel
+            // 
+            resources.ApplyResources(cyborgCounterLabel, "cyborgCounterLabel");
+            cyborgCounterLabel.Name = "cyborgCounterLabel";
+            // 
+            // alienCounterLabel
+            // 
+            resources.ApplyResources(alienCounterLabel, "alienCounterLabel");
+            alienCounterLabel.Name = "alienCounterLabel";
+            // 
+            // Game
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(alienCounterLabel);
+            Controls.Add(cyborgCounterLabel);
+            Controls.Add(alienFront);
+            Controls.Add(cyborgFront);
+            Controls.Add(alienBottomCorner);
+            Controls.Add(alienTopCorner);
+            Controls.Add(alienBottom);
+            Controls.Add(alienTop);
+            Controls.Add(cyborgBottomCorner);
+            Controls.Add(cyborgTopCorner);
+            Controls.Add(cyborgBottom);
+            Controls.Add(cyborgTop);
+            Controls.Add(ball);
+            Controls.Add(cyborg);
+            Controls.Add(alien);
+            DoubleBuffered = true;
+            Name = "Game";
+            KeyDown += Game_KeyDown;
+            KeyUp += Game_KeyUp;
+            ((System.ComponentModel.ISupportInitialize)alien).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cyborg).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ball).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cyborgTop).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cyborgBottom).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cyborgTopCorner).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cyborgBottomCorner).EndInit();
+            ((System.ComponentModel.ISupportInitialize)alienBottomCorner).EndInit();
+            ((System.ComponentModel.ISupportInitialize)alienTopCorner).EndInit();
+            ((System.ComponentModel.ISupportInitialize)alienBottom).EndInit();
+            ((System.ComponentModel.ISupportInitialize)alienTop).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cyborgFront).EndInit();
+            ((System.ComponentModel.ISupportInitialize)alienFront).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox alien;
+        private PictureBox cyborg;
+        private System.Windows.Forms.Timer Timer;
+        private PictureBox ball;
+        private PictureBox cyborgTop;
+        private PictureBox cyborgBottom;
+        private PictureBox cyborgTopCorner;
+        private PictureBox cyborgBottomCorner;
+        private PictureBox alienBottomCorner;
+        private PictureBox alienTopCorner;
+        private PictureBox alienBottom;
+        private PictureBox alienTop;
+        private PictureBox cyborgFront;
+        private PictureBox alienFront;
+        private Label cyborgCounterLabel;
+        private Label alienCounterLabel;
+    }
+}
---
+++ WinFormsAppPingPong/Game.cs
@@ -0,0 +1,219 @@
+namespace WinFormsAppPingPong
+{
+    public partial class Game : Form
+    {
+        public int ballSpeedX = 5;
+        public int ballSpeedY = 5;
+        public int playerSpeed = 3;
+        bool alienUp, alienDown;
+        bool cyborgUp, cyborgDown;
+        int alienCounter, cyborgCounter;
+        bool ballMove = true;
+
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        public Game(int ballSpeedX, int ballSpeedY)
+        {
+            this.ballSpeedX = ballSpeedX;
+            this.ballSpeedY = ballSpeedY;
+
+            InitializeComponent();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (ballMove)
+            {
+                ball.Top -= ballSpeedY;
+                ball.Left -= ballSpeedX;
+
+                if (ball.Top <= 0 || ball.Bottom >= ClientSize.Height)
+                {
+                    ballSpeedY = -ballSpeedY;
+                }
+            }
+
+            if (ball.Left > ClientSize.Width)
+            {
+                ++cyborgCounter;
+                NewRound();
+            }
+            if (ball.Right < 0)
+            {
+                ++alienCounter;
+                NewRound();
+            }
+
+            if (alienDown)
+            {
+                MoveAlienBy(playerSpeed);
+            }
+
+            if (alienUp)
+            {
+                MoveAlienBy(-playerSpeed);
+            }
+
+            if (cyborgDown)
+            {
+                MoveCyborgBy(playerSpeed);
+            }
+            if (cyborgUp)
+            {
+                MoveCyborgBy(-playerSpeed);
+            }
+
+            if (alien.Top < 0)
+            {
+                MoveAlienBy(playerSpeed);
+            }
+            if (cyborg.Top < 0)
+            {
+                MoveCyborgBy(playerSpeed);
+            }
+
+            if (alien.Bottom > ClientSize.Height)
+            {
+                MoveAlienBy(-playerSpeed);
+            }
+            if (cyborg.Bottom > ClientSize.Height)
+            {
+                MoveCyborgBy(-playerSpeed);
+            }
+
+            alienCounterLabel.Text = alienCounter.ToString();
+            cyborgCounterLabel.Text = cyborgCounter.ToString();
+
+            CheckCollisions();
+        }
+
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                alienDown = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                alienUp = true;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                cyborgUp = true;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                cyborgDown = true;
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                ballMove = true;
+            }
+        }
+
+        private void Game_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                alienDown = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                alienUp = false;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                cyborgUp = false;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                cyborgDown = false;
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                ballMove = false;
+            }
+        }
+
+        private void MoveAlienBy(int speed)
+        {
+            alien.Top += speed;
+            alienBottom.Top += speed;
+            alienBottomCorner.Top += speed;
+            alienTop.Top += speed;
+            alienTopCorner.Top += speed;
+            alienFront.Top += speed;
+        }
+
+        private void MoveCyborgBy(int speed)
+        {
+            cyborg.Top += speed;
+            cyborgBottom.Top += speed;
+            cyborgBottomCorner.Top += speed;
+            cyborgTop.Top += speed;
+            cyborgTopCorner.Top += speed;
+            cyborgFront.Top += speed;
+        }
+
+        private void CheckCollisions()
+        {
+            if (ball.Bounds.IntersectsWith(cyborgFront.Bounds))
+            {
+                ballSpeedX = -ballSpeedX;
+                //ball.Left -= ballSpeedX;
+                return;
+            }
+            if (ball.Bounds.IntersectsWith(cyborgTop.Bounds) || ball.Bounds.IntersectsWith(cyborgBottom.Bounds))
+            {
+                ballSpeedY = -ballSpeedY;
+                //ball.Top -= ballSpeedY;
+                return;
+            }
+            if (ball.Bounds.IntersectsWith(cyborgTopCorner.Bounds) || ball.Bounds.IntersectsWith(cyborgBottomCorner.Bounds))
+            {
+                ballSpeedX = -ballSpeedX;
+                ballSpeedY = -ballSpeedY;
+                //ball.Left -= ballSpeedX + ball.Left;
+                //ball.Top -= ballSpeedY + ball.Bottom;
+                return;
+            }
+
+            if (ball.Bounds.IntersectsWith(alienFront.Bounds))
+            {
+                ballSpeedX = -ballSpeedX;
+                //ball.Left += ballSpeedX;
+                return;
+            }
+            if (ball.Bounds.IntersectsWith(alienTop.Bounds) || ball.Bounds.IntersectsWith(alienBottom.Bounds))
+            {
+                ballSpeedY = -ballSpeedY;
+                //ball.Top += ballSpeedY;
+                return;
+            }
+            if (ball.Bounds.IntersectsWith(alienTopCorner.Bounds) || ball.Bounds.IntersectsWith(alienBottomCorner.Bounds))
+            {
+                ballSpeedX = -ballSpeedX;
+                ballSpeedY = -ballSpeedY;
+                //ball.Left += ballSpeedX - ball.Right;
+                //ball.Top += ballSpeedY - ball.Bottom;
+                return;
+            }
+        }
+
+        private void NewRound()
+        {
+            ballSpeedX = -ballSpeedX;
+
+            Random random = new Random();
+            ball.Top = random.Next(ClientSize.Height / 3, 2 * ClientSize.Height / 3);
+            ball.Left = random.Next(ClientSize.Width / 3, 2 * ClientSize.Width / 3);
+        }
+    }
+}
