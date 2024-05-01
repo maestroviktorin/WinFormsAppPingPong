f136c33b89f5ae424a436ed29fbc6ef05e2076da
Viktor Starodubtsev <viktorst2005@gmail.com>
2024-05-01 14:36:03 +0400

    Menu redesign started.

--- WinFormsAppPingPong/Menu.Designer.cs
+++ WinFormsAppPingPong/Menu.Designer.cs
@@ -28,148 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuForm));
+            easy = new Button();
+            medium = new Button();
+            hard = new Button();
             label1 = new Label();
             label2 = new Label();
-            pictureBox1 = new PictureBox();
-            pictureBox2 = new PictureBox();
+            label3 = new Label();
+            label4 = new Label();
             button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            label5 = new Label();
+            label6 = new Label();
             SuspendLayout();
+            // 
+            // easy
+            // 
+            easy.Anchor = AnchorStyles.None;
+            easy.BackColor = Color.Ivory;
+            easy.Font = new Font("Cascadia Mono", 25.92F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            easy.ForeColor = Color.CornflowerBlue;
+            easy.Location = new Point(13, 324);
+            easy.Margin = new Padding(4, 3, 4, 3);
+            easy.Name = "easy";
+            easy.Size = new Size(103, 85);
+            easy.TabIndex = 4;
+            easy.Text = "😊";
+            easy.UseVisualStyleBackColor = false;
+            easy.Click += button1_Click;
+            // 
+            // medium
+            // 
+            medium.Anchor = AnchorStyles.None;
+            medium.BackColor = Color.Ivory;
+            medium.Font = new Font("Cascadia Mono", 25.92F, FontStyle.Bold);
+            medium.ForeColor = Color.Orange;
+            medium.Location = new Point(124, 324);
+            medium.Margin = new Padding(4, 3, 4, 3);
+            medium.Name = "medium";
+            medium.Size = new Size(103, 85);
+            medium.TabIndex = 5;
+            medium.Text = "🙃";
+            medium.UseVisualStyleBackColor = false;
+            medium.Click += button2_Click;
+            // 
+            // hard
+            // 
+            hard.Anchor = AnchorStyles.None;
+            hard.BackColor = Color.Ivory;
+            hard.Font = new Font("Cascadia Mono", 25.92F, FontStyle.Bold);
+            hard.ForeColor = Color.Crimson;
+            hard.Location = new Point(235, 324);
+            hard.Margin = new Padding(4, 3, 4, 3);
+            hard.Name = "hard";
+            hard.Size = new Size(103, 85);
+            hard.TabIndex = 6;
+            hard.Text = "☠️";
+            hard.UseVisualStyleBackColor = false;
+            hard.Click += button3_Click;
             // 
             // label1
             // 
-            label1.Anchor = AnchorStyles.Top;
-            label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
+            label1.Dock = DockStyle.Top;
             label1.Font = new Font("Cascadia Mono", 36.288F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.Ivory;
-            label1.Location = new Point(38, 7);
+            label1.Location = new Point(0, 0);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(549, 64);
-            label1.TabIndex = 0;
+            label1.Size = new Size(905, 83);
+            label1.TabIndex = 7;
             label1.Text = "CYBORGS VS. ALIENS";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
-            label2.Anchor = AnchorStyles.Top;
-            label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
-            label2.Font = new Font("Cascadia Mono", 13.8239994F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Dock = DockStyle.Top;
+            label2.Font = new Font("Cascadia Mono", 15F, FontStyle.Bold | FontStyle.Italic);
             label2.ForeColor = Color.Ivory;
-            label2.Location = new Point(253, 66);
+            label2.Location = new Point(0, 83);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(111, 25);
-            label2.TabIndex = 1;
+            label2.Size = new Size(905, 35);
+            label2.TabIndex = 8;
             label2.Text = "PING-PONG";
             label2.TextAlign = ContentAlignment.TopCenter;
             // 
-            // pictureBox1
+            // label3
             // 
-            pictureBox1.Anchor = AnchorStyles.Top;
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(147, 68);
-            pictureBox1.Margin = new Padding(3, 2, 3, 2);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(102, 93);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 2;
-            pictureBox1.TabStop = false;
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Ivory;
+            label3.Location = new Point(12, 265);
+            label3.Name = "label3";
+            label3.Size = new Size(336, 56);
+            label3.TabIndex = 9;
+            label3.Text = "      CREATE NEW GAME\r\nAND DEFEND YOUR COMPUTER...";
             // 
-            // pictureBox2
+            // label4
             // 
-            pictureBox2.Anchor = AnchorStyles.Top;
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(370, 66);
-            pictureBox2.Margin = new Padding(3, 2, 3, 2);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(102, 93);
-            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.TabIndex = 3;
-            pictureBox2.TabStop = false;
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Cascadia Mono SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Ivory;
+            label4.Location = new Point(518, 293);
+            label4.Name = "label4";
+            label4.Size = new Size(360, 28);
+            label4.TabIndex = 10;
+            label4.Text = "...OR INVADE ANOTHER COMPUTER";
             // 
             // button1
             // 
-            button1.Anchor = AnchorStyles.Bottom;
-            button1.BackColor = Color.Ivory;
             button1.Font = new Font("Cascadia Mono", 25.92F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.ForeColor = Color.CornflowerBlue;
-            button1.Location = new Point(38, 205);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Location = new Point(533, 324);
             button1.Name = "button1";
-            button1.Size = new Size(116, 68);
-            button1.TabIndex = 4;
-            button1.Text = "😊";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
+            button1.Size = new Size(325, 85);
+            button1.TabIndex = 11;
+            button1.Text = "JOIN";
+            button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // label5
             // 
-            button2.Anchor = AnchorStyles.Bottom;
-            button2.BackColor = Color.Ivory;
-            button2.Font = new Font("Cascadia Mono", 25.92F, FontStyle.Bold);
-            button2.ForeColor = Color.Orange;
-            button2.Location = new Point(284, 205);
-            button2.Margin = new Padding(3, 2, 3, 2);
-            button2.Name = "button2";
-            button2.Size = new Size(116, 68);
-            button2.TabIndex = 5;
-            button2.Text = "🙃";
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += button2_Click;
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Cascadia Mono", 47.808F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Fuchsia;
+            label5.Location = new Point(102, 156);
+            label5.Name = "label5";
+            label5.Size = new Size(155, 109);
+            label5.TabIndex = 12;
+            label5.Text = "🤖";
             // 
-            // button3
+            // label6
             // 
-            button3.Anchor = AnchorStyles.Bottom;
-            button3.BackColor = Color.Ivory;
-            button3.Font = new Font("Cascadia Mono", 25.92F, FontStyle.Bold);
-            button3.ForeColor = Color.Crimson;
-            button3.Location = new Point(520, 205);
-            button3.Margin = new Padding(3, 2, 3, 2);
-            button3.Name = "button3";
-            button3.Size = new Size(116, 68);
-            button3.TabIndex = 6;
-            button3.Text = "\U0001f978";
-            button3.UseVisualStyleBackColor = false;
-            button3.Click += button3_Click;
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Cascadia Mono", 47.808F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Chartreuse;
+            label6.Location = new Point(617, 156);
+            label6.Name = "label6";
+            label6.Size = new Size(155, 109);
+            label6.TabIndex = 13;
+            label6.Text = "👽";
             // 
             // MenuForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
-            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImage = Properties.Resources.background;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(704, 376);
-            Controls.Add(button3);
-            Controls.Add(button2);
+            ClientSize = new Size(905, 526);
+            Controls.Add(label6);
+            Controls.Add(label5);
             Controls.Add(button1);
-            Controls.Add(pictureBox2);
-            Controls.Add(pictureBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Controls.Add(hard);
+            Controls.Add(medium);
+            Controls.Add(easy);
             DoubleBuffered = true;
-            Margin = new Padding(3, 2, 3, 2);
+            Margin = new Padding(4, 3, 4, 3);
             Name = "MenuForm";
             Text = "Menu";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
+        private Button easy;
+        private Button medium;
+        private Button hard;
         private Label label1;
         private Label label2;
-        private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
+        private Label label3;
+        private Label label4;
         private Button button1;
-        private Button button2;
-        private Button button3;
+        private Label label5;
+        private Label label6;
     }
 }
--- WinFormsAppPingPong/Properties/Resources.Designer.cs
+++ WinFormsAppPingPong/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace WinFormsAppPingPong.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap background {
+            get {
+                object obj = ResourceManager.GetObject("background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream easy {
